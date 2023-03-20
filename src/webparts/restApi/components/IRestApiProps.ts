f99d352974619ca54f17cb8955a29941c6b3ba48
Harry <nthai7@cmcglobal.vn>
2023-03-20 11:08:51 +0700

    first commit

---
+++ src/webparts/restApi/components/IRestApiProps.ts
@@ -0,0 +1,7 @@
+export interface IRestApiProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/restApi/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IRestApiWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'RestApiWebPartStrings' {
+  const strings: IRestApiWebPartStrings;
+  export = strings;
+}
